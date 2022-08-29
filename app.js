ca129382d764279a24c8c016257c889ea3918753
alex-arango47 <alexis.arango21@gmail.com>
2022-08-29 20:57:53 -0300

    Adding some styles, adding 404 view, adding structure for JS elements

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const mainRoutes = require('./routes/mainRoutes');
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/', mainRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404');
+    next();
+});
 
 app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
