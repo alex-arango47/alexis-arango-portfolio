136956fc564ba4baf991ae2b5acae0d400692d62
alex-arango47 <alexis.arango21@gmail.com>
2022-08-29 21:23:02 -0300

    adding more styling

--- public/js/burger.js
+++
@@ -1,8 +0,0 @@
-const burgerMenu = document.getElementById('burger-menu');
-
-const overlay = document.getElementById('menu');
-
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
-});
