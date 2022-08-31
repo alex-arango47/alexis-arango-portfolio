8b677a9e5a1ca109f4b8cc1c5686d668594be2d3
alex-arango47 <alexis.arango21@gmail.com>
2022-08-30 23:51:22 -0300

    Changes to styles

--- public/js/backToBtn.js
+++ public/js/backToBtn.js
@@ -2,6 +2,6 @@ window.addEventListener('scroll', () => {
      if(window.pageYOffset > 800){
         document.querySelector(".backToTop").style = "display: flex";
      } else if (window.pageYOffset < 800){
-        document.querySelector(".backToTop").style = "display: none";
+        document.querySelector(".backToTop").style = "display; none";
      }
 });
