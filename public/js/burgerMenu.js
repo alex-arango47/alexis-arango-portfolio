d47453cdcb30a8841124d489c6d9aae125561253
alex-arango47 <alexis.arango21@gmail.com>
2022-09-02 20:58:55 -0300

    Changes

--- public/js/burgerMenu.js
+++ public/js/burgerMenu.js
@@ -28,5 +28,47 @@ burgerBtn.addEventListener("click", (event) => {
          menuNavBar.classList.remove("navSection-Mobile_Hidden")
          menuNavBar.classList.add("navSection-Mobile_Show")
     }
+    if(menuNavBar.classList.contains("navSection-Mobile_Show")){
+        const buttons = document.querySelector("#navSection-Bar_Mobile");
+        buttons.addEventListener("click", () => {
+            burgerMenu.classList.remove("burgerMenu-Main_Show")
+            burgerMenu.classList.add("burgerMenu-Main_FadeOut")
+            menuNavBar.classList.remove("navSection-Mobile_Show")
+            menuNavBar.classList.add("navSection-Mobile_FadeOut")
+             setTimeout(() => {
+                burgerMenu.classList.add("burgerMenu-Main_Hidden")
+                burgerMenu.classList.remove("burgerMenu-Main_FadeOut")
+                menuNavBar.classList.add("navSection-Mobile_Hidden")
+                menuNavBar.classList.remove("navSection-Mobile_FadeOut")
+             }, 450)
+         })
+    }
 })
+
+
+
+
+
+    // if(burgerMenu.classList.contains("burgerMenu-Main_Show")){
+    //     const buttons = document.querySelector("#navSection-Bar_Mobile");
+    //     buttons.addEventListener("click", () => {
+    //         burgerMenu.classList.remove("burgerMenu-Main_Show")
+    //         burgerMenu.classList.add("burgerMenu-Main_FadeOut")
+    //         setTimeout(() => {
+    //             burgerMenu.classList.add("burgerMenu-Main_Hidden")
+    //             burgerMenu.classList.remove("burgerMenu-Main_FadeOut")
+    //         }, 450)
+    //     })
+    //     if(menuNavBar.classList.contains("navSection-Mobile_Show")){
+    //         menuNavBar.classList.remove("navSection-Mobile_Show")
+    //         menuNavBar.classList.add("navSection-Mobile_FadeOut")
+    //         setTimeout(() => {
+    //             menuNavBar.classList.add("navSection-Mobile_Hidden")
+    //             menuNavBar.classList.remove("navSection-Mobile_FadeOut")
+    //         }, 450)
+    //     }
+    // }
+
+
+
 
