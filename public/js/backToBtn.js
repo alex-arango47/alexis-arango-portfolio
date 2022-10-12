19a7ee5d6350f46d4eec98789a662256034dfb80
alex-arango47 <alexis.arango21@gmail.com>
2022-10-12 18:58:49 +1300

    some changes to styling, finishing first section home of the portfolio

--- public/js/backToBtn.js
+++ public/js/backToBtn.js
@@ -3,10 +3,10 @@ window.onscroll = (event) => {
    const main = document.querySelector("#backToTop-Main");
    const button = document.querySelector("#backToTop-Main_Btn");
 
-   if(window.pageYOffset > 800){
+   if(window.pageYOffset > 450){
       button.classList.add("backToTop");
       main.classList.remove("backToTopGhost")
-   } else if (window.pageYOffset < 800 && button.classList.contains("backToTop")){
+   } else if (window.pageYOffset < 450 && button.classList.contains("backToTop")){
       button.classList.add("backToTopFadeOut");
       button.classList.remove("backToTop");
       setTimeout(() => {
